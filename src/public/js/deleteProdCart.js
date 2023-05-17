48e1dd74cb8dfc610f6c9ebb31128b60955fad39
Nicolas Doffo <fotoestudio9@hotmail.com>
2023-05-17 18:18:27 -0300

    Actualizando2

--- src/public/js/deleteProdCart.js
+++ src/public/js/deleteProdCart.js
@@ -1,7 +1,7 @@
 let idCart = document.getElementById('idCart').innerText.split(' ')[2]
 
 function deleteProduct (idProduct){
-    fetch(`ecommercecoderhouse2-production.up.railway.app/api/carts/${idCart}/products/${idProduct}`, {
+    fetch(`https://ecommercecoderhouse2-production.up.railway.app//api/carts/${idCart}/products/${idProduct}`, {
     method: "DELETE",
     
   }).then(function(response) {
